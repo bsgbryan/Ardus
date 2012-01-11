47dc160b2be802219be7c13723d9a324b07c487e
Bryan Maynard <bsgbryan@gmail.com>
2012-01-11 17:08:16 -0600

    Updated the prototype example

--- examples/modules/prototype.js
+++ examples/modules/prototype.js
@@ -1,9 +1,11 @@
+var profile = require('../../lib/ardus').profiler;
+
 function Bob(name) {
   this.name = name;
 };
 
-Bob.prototype.is = function foo(adjective) {
+Bob.prototype.is = profile(function foo(adjective) {
   console.log(this.name + ' is ' + adjective);
-};
+});
 
 module.exports = Bob;
