5a39b56736b000236110fa7b426d78e50edd3263
Bryan Maynard <bsgbryan@gmail.com>
2012-01-11 16:27:56 -0600

    Tap can now be profiled

--- examples/prototype.js
+++ examples/prototype.js
@@ -4,9 +4,6 @@ ardus.global('profiler').profile();
 
 var Bob = require('./modules/prototype'),
     bob = new Bob('Dave');
-
-console.log("\nMy prototype: ", bob.prototype);
-
 bob.is('cool');
 
 process.on('exit', function () {
